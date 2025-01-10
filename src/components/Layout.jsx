c981bd0b08fe9dbd408cca398c83add6d2c8a01e
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 15:43:35 +0100

    made some changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Layout = ({ children }) => {
   return (
-    <div className="bg-white p-6 grid grid-cols-form  max-w-[94rem] mx-auto min-h-[60rem] md:grid-cols-1 mid:mx-2rem mid:p-0 sma:bg-transparent  sma:relative rounded-3xl sma:min-h-screen">
+    <div className="bg-white p-6 grid grid-cols-form  max-w-[94rem] mx-auto min-h-[60rem] mid:grid-cols-1 mid:mx-2rem mid:p-0 sma:bg-transparent  sma:relative rounded-3xl sma:min-h-screen">
       {children}
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Layout } from '../components/Layout'
 import { Sidebar } from '../components/Sidebar/Sidebar'
+import { Layout } from '../components/Layout'
 import { Form } from '../components/Form/Form'
 
 const HomePage = () => {
