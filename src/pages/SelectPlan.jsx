d026f74395ff847e980b6e97fdcfc4b7258ddb6d
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 14:31:35 +0100

    imported the sidebar and plansform component in the selectplan component

--- src/pages/SelectPlan.jsx
+++ src/pages/SelectPlan.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Layout } from '../components/Layout'
 import { Sidebar } from '../components/Sidebar/Sidebar'
+import PlansForm from '../components/Plans/PlansForm'
 
 export const SelectPlan = () => {
   return (
     <Layout>
         <Sidebar />
-        
+        <PlansForm />
     </Layout>
   )
 }
