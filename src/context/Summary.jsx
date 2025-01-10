80a61f0be307302aa5f83603e2f0f4a5420561ce
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 14:58:57 +0100

    imported the sidebar and summary component in the add ons component

--- src/context/Summary.jsx
+++ src/context/Summary.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Layout } from '../components/Layout'
 import { Sidebar } from '../components/Sidebar/Sidebar'
+import SummaryForm from '../components/Summary/SummaryForm'
 
 export const Summary = () => {
   return (
     <Layout>
         <Sidebar />
-        
+        <SummaryForm />
     </Layout>
   )
 }
