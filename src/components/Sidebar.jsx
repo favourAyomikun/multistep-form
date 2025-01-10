684f631ebc0fb03dbb73fbaab39ccd7b46e12c41
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 11:56:42 +0100

    created other components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Sidebar = () => {
+  return (
+    <nav className=" flex flex-col gap-12 bg-[url(/bg-sidebar-desktop.svg)] py-16 px-14 h-[56.8rem] mid:bg-[url(/bg-sidebar-mobile.svg)] mid:flex-row mid:justify-center mid:h-[17.2rem] mid:bg-center mid:bg-no-repeat sma:py-12 sma:items-baseline">
+        
+    </nav>
+  );
+};
