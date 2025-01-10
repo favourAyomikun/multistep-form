314b0f2642684688a8662d426444ff96c3d0826a
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 14:43:25 +0100

    imported the sidebar and addons form component in the add ons component

--- src/pages/AddOns.jsx
+++ src/pages/AddOns.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Layout } from '../components/Layout'
 import { Sidebar } from '../components/Sidebar/Sidebar'
+import AddOnsForm from '../components/AddOns/AddOnsForm'
 
 export const AddOns = () => {
   return (
     <Layout>
         <Sidebar />
-        
+        <AddOnsForm />
     </Layout>
   )
 }
