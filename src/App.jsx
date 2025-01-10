8de9c598937194bdab26384e913d75c0fa3c2283
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 11:47:24 +0100

    created a layout and homepage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div className=" bg-[#eff5ff] py-[10.5rem] md:py-0">
       heeyeyeyy
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Layout = () => {
+  return (
+    <div className="bg-white p-6 grid grid-cols-form  max-w-[94rem] mx-auto min-h-[60rem] mid:grid-cols-1 mid:mx-2rem mid:p-0 sma:bg-transparent  sma:relative rounded-3xl sma:min-h-screen">
+      {children}
+    </div>
+  );
+};
