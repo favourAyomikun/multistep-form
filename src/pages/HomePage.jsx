49b744baa98738ff68abf01ee1bb879183a71ad6
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 13:54:55 +0100

    imported the sidebar and form component in the homepage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Layout } from '../components/Layout'
 import { Sidebar } from '../components/Sidebar'
 import { Form } from '../components/Form'
-import { useForm } from "../contexts/FormContexts";
 
 const HomePage = () => {
   return (
