c33f7de5c062e5be071bf478206799106c832311
Oshoaro Favour <oshoarofavour@gmail.com>
2025-01-10 15:10:01 +0100

    created a spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <img src="/Rolling-1s-200px.svg" alt="spinner" />
+      </div>
+    );
+  }
+  
+  export default Spinner;
